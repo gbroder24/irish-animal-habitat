3941f5dc4f1dd18364614cfe78633000c78c6484
gbroder24 <garybroderick82@gmail.com>
2024-05-24 11:47:56 +0000

    Add html for boxes and style. Add javascript fu flip function and click event listener to toggle flip

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,12 @@
-console.log("Connected!");
+function boxClicked (event){
+    let div = this;
+    console.log("Clicked")
+    this.classList.toggle('box-inner');
+}
+
+let classBox = document.getElementsByClassName("box");
+
+
+for(i=0; i<classBox.length; i++){
+    classBox[i].addEventListener('click', boxClicked);
+}
