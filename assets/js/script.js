65f2924a1ef26b9a0712d9bcd2c578751e08cd3a
gbroder24 <garybroderick82@gmail.com>
2024-05-24 14:58:40 +0000

    Add store logic to boxClicked function to store the state of the card

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,29 @@
-function boxClicked (event){
-    let div = this;
-    console.log("Clicked")
-    this.classList.toggle('box-inner');
-}
+let flippedCard = false;
+let cardOne;
+let cardTwo;
 
 let classBox = document.getElementsByClassName("box");
-
 
 for(i=0; i<classBox.length; i++){
     classBox[i].addEventListener('click', boxClicked);
 }
+
+/**
+ * 
+ * The function checks if this box was clicked.
+ * If clicked, flip card and store the state of the the card. 
+ */
+function boxClicked (event){
+    let div = this;
+    console.log("Clicked")
+    this.classList.toggle('box-inner');
+
+    if (!flippedCard){
+        flippedCard = true;
+        cardOne = this;
+    } else {
+        flippedCard = false;
+        cardTwo = this;
+    }
+}
+
