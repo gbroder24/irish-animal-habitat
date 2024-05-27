7d6d354c6708bc148fdcc0b64aa14dcc035ed390
gbroder24 <garybroderick82@gmail.com>
2024-05-27 14:31:38 +0000

    Add js code for cards that do not match

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,14 +2,18 @@ let flippedCard = false;
 let cardOne;
 let cardTwo;
 
+let classBox = document.getElementsByClassName("box");
 
-
-
+for(i=0; i<classBox.length; i++){
+    classBox[i].addEventListener('click', boxClicked);
+}
 
 /**
  * 
- * The function checks if this box was clicked.
- * If clicked, flip card and store the state of the the card. 
+ * The function checks if this card was clicked.
+ * If clicked, flip card and store the state of the the card.
+ * If cards match, stay flipped. If cards don't match flip back.
+ * Code used from https://www.youtube.com/watch?v=m8nmh3_lejc&list=PLLX1I3KXZ-YH-woTgiCfONMya39-Ty8qw&index=8 tutorial
  */
 function boxClicked (event){
     let div = this;
@@ -23,19 +27,18 @@ function boxClicked (event){
         //second click
         flippedCard = false;
         cardTwo = this;
-    }
-
+    
     //do cards match
     if(cardOne.dataset.image === cardTwo.dataset.image){
         //cards match
         cardOne.removeEventListener('click', boxClicked);
         cardTwo.removeEventListener('click', boxClicked);
-        console.log('Cards match!');
-    } 
-}
-
-let classBox = document.getElementsByClassName("box");
-
-for(i=0; i<classBox.length; i++){
-    classBox[i].addEventListener('click', boxClicked);
+    } else {
+        //cards do not match
+        setTimeout(() => {
+            cardOne.classList.remove('box-inner');
+            cardTwo.classList.remove('box-inner');
+        }, 1000);
+    }
+ }
 }
