5065256fcf665f40709f88c16aed42fb4a3895fa
gbroder24 <garybroderick82@gmail.com>
2024-06-03 16:21:33 +0000

    Add a page reload function to load a new game and update multi line function comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+//Declare Global Variables
 let flippedCard = false;
 let lockBoard = false;
 let cardOne;
@@ -6,15 +7,17 @@ let cardTwo;
 let cardsChosen = [];
 let cardsWon = [];
 
+
 let pairsFound = document.getElementById('result');
 let congratsModal = document.getElementById('congrats-modal');
 let closeCongratsModal = document.getElementById('close');
 let newGame = document.getElementById('yes-btn');
 let noNewGame = document.getElementById('no-btn');
 
-
+let gameArea = document.getElementsByClassName("game-area");
 let classBox = document.getElementsByClassName("box");
 
+// This for loop iterates over the classBox array. It adds an event listener to each card and listens for a click.
 for(i=0; i<classBox.length; i++){
     classBox[i].addEventListener('click', boxClicked);
 }
@@ -41,33 +44,34 @@ function boxClicked (event){
           }
     }
 
-
-
 /**
- * The functions below checks if cards match, remove event listener and stay flipped. If cards don't match flip back.
+ * The function checks the cards for a match. If they match call function disableCards().
+ * Use push method on the chosen pair of cards and store in cardsWon variable.
+ * If cardsWon length is strictly equal to integer number 6 call display congrats modal.
+ * Else unflip chosen cards and empty cards chosen array.
  * Code used from https://youtu.be/m8nmh3_lejc?si=2rHCCisfFgYSHW0W tutorial
  */
 function checkForMatch(){
     if(cardOne.dataset.image === cardTwo.dataset.image){
         disableCards();
-
         cardsWon.push(cardsChosen);
         pairsFound.innerHTML = cardsWon.length;
 
         if(cardsWon.length === 6){
             displayCongratsModal();
-            
         }
         cardsChosen = [];
 
-
-        
     } else {
         unFlipCards();
         cardsChosen = [];
     }
 }
 
+/**
+ * The function removes the click event listener from cards that have been paired
+ * and the function resetBoard is called.
+ */
 function disableCards(){
     cardOne.removeEventListener('click', boxClicked);
     cardTwo.removeEventListener('click', boxClicked);
@@ -75,6 +79,10 @@ function disableCards(){
     resetBoard();
 }
 
+/**
+ * The function executes when a pair of flipped cards do not match.
+ * It has a time delay of 1.5 secs to unflip.
+ */
 function unFlipCards(){
     lockBoard = true;
 
@@ -99,17 +107,18 @@ function resetBoard(){
 }
 
 /**
- * This function is an Immediately Invoked Function Expression.
  * It shuffles the cards and assigns each card
  * a random integer.
  * Code used from https://youtu.be/20k84LS1LZY?si=Oy7ZmA21I4U-_UPy tutorial
  */
-(function shuffle(){
+function shuffle(){
     for(i=0; i<classBox.length; i++){
         let randomPos = Math.floor(Math.random() * 12);
         classBox[i].style.order = randomPos;
     }
-})()
+}
+
+shuffle();
 
 /**This function displays the congratulations modal
  * Code used from https://www.w3schools.com/howto/howto_css_modals.asp tutorial
@@ -122,13 +131,13 @@ function displayCongratsModal() {
  * Code used from https://www.w3schools.com/howto/howto_css_modals.asp tutorial
  */
 function closeModalCongrats() {
-    console.log("Modal Closed!")
     congratsModal.style.display = "none";
+    location.reload();
 }
-
 
 let classButton = document.getElementsByClassName("button");
 
+// This for loop iterates over the classButton array. It adds an event listener to each button in the congrats modal and listens for a click.
 for(j=0; j<classButton.length; j++){
     classButton[j].addEventListener('click', closeModalCongrats);
 }
